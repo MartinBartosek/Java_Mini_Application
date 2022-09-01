e67681367b0ee5d4e67b5f63cf85a58f268e0142
martin.bartosek <martin.bartosek@3pillarglobal.com>
2022-09-01 08:37:56 +0200

    Initial commit.

---
+++ src/test/java/com/pillarglobal/miniApp/MiniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.miniApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
