4e00c117208f471b95e26c63df4394878f0c5571
martin.bartosek <martin.bartosek@3pillarglobal.com>
2022-09-20 11:31:29 +0200

    New tests.

--- src/test/java/com/pillarglobal/miniApp/services/CalculatorServiceImplTest.java
+++ src/test/java/com/pillarglobal/miniApp/services/CalculatorServiceImplTest.java
@@ -1,20 +1,51 @@
 package com.pillarglobal.miniApp.services;
 
 import com.pillarglobal.miniApp.Calculator.controllers.services.CalculatorServiceImpl;
+import com.pillarglobal.miniApp.Calculator.exceptions.MultipleOperatorException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
 class CalculatorServiceImplTest {
 
     @InjectMocks
     private CalculatorServiceImpl calculatorService;
 
+    @BeforeEach
+    void beforeEach() {
+    }
+
     @Test
-    void calculateExpression() {
+    void solveExpressionExpression() throws MultipleOperatorException {
+        //given
+        String testExpression = "2*3-2";
+        //when
+        int result = calculatorService.solveExpression(testExpression);
+        //then
+        assertThat(result).isEqualTo(4);
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    void solveExpressionShouldThrowExceptionWhenSameOperatorsAreUsedInARow() {
+        //given
+        String testExpression = "3+++++5";
+        //then
+        assertThrows(MultipleOperatorException.class, () -> calculatorService.solveExpression(testExpression));
     }
 
     @Test
     void hasPrecedence() {
+
     }
 
     @Test
